8bbc1b19047c905ffbb664940533f61777c24a62
- <->
2018-09-27 03:45:13 +0700

    Update training project: Back end L02

--- danhsachdm.php
+++ danhsachdm.php
@@ -1,124 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Vietpro Mobile Shop</title>
-
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/datepicker3.css" rel="stylesheet">
-        <link href="css/bootstrap-table.css" rel="stylesheet">
-        <link href="css/styles.css" rel="stylesheet">
-
-        <!--Icons-->
-        <script src="js/lumino.glyphs.js"></script>
-
-        <!--[if lt IE 9]>
-        <script src="js/html5shiv.js"></script>
-        <script src="js/respond.min.js"></script>
-        <![endif]-->
-
-    </head>
-
-    <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"><span>VIETPROSHOP</span>Admin</a>
-                    <ul class="user-menu">
-                        <li class="dropdown pull-right">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg><span style="color: white;">Xin chào, </span> Thành viên <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Thông tin thành viên</a></li>
-                                <li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Cài đặt</a></li>
-                                <li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Đăng xuất</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-
-            </div><!-- /.container-fluid -->
-        </nav>
-
-        <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
-            <form role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Tìm kiếm">
-                </div>
-            </form>
-            <ul class="nav menu">
-                <li><a href="index.html"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Trang chủ quản trị</a></li>
-                <li class="parent ">
-                    <a href="#">
-                        <span data-toggle="collapse" href="#sub-item-1"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Quản lý thành viên
-                    </a>
-                    <ul class="children collapse" id="sub-item-1">					
-                        <li>
-                            <a href="#">
-                                <svg class="glyph stroked plus sign"><use xlink:href="#stroked-plus-sign"/></svg>
-                                Thêm mới
-                            </a>
-                        </li>
-                    </ul>			
-                </li>
-                <li class="parent active">
-                    <a href="#">
-                        <span data-toggle="collapse" href="#sub-item-2"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Quản lý danh mục
-                    </a>
-                    <ul class="children collapse" id="sub-item-2">					
-                        <li>
-                            <a class="" href="#">
-                                <svg class="glyph stroked plus sign"><use xlink:href="#stroked-plus-sign"/></svg> Thêm mới
-                            </a>
-                        </li>
-                    </ul>			
-                </li>
-                <li class="parent ">
-                    <a href="#">
-                        <span data-toggle="collapse" href="#sub-item-3"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Quản lý sản phẩm
-                    </a>
-                    <ul class="children collapse" id="sub-item-3">					
-                        <li>
-                            <a class="" href="#">
-                                <svg class="glyph stroked plus sign"><use xlink:href="#stroked-plus-sign"/></svg> Thêm mới
-                            </a>
-                        </li>
-                    </ul>				
-                </li>
-                <li class="parent ">
-                    <a href="#">
-                        <span data-toggle="collapse" href="#sub-item-4"><svg class="glyph stroked two messages"><use xlink:href="#stroked-two-messages"/></svg></span> Quản lý bình luận
-                    </a>			
-                </li>
-                <li class="parent ">
-                    <a href="#">
-                        <span data-toggle="collapse" href="#sub-item-5"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Quản lý quảng cáo
-                    </a>
-                    <ul class="children collapse" id="sub-item-5">
-                        <li>
-                            <a class="" href="#">
-                                <svg class="glyph stroked plus sign"><use xlink:href="#stroked-plus-sign"/></svg> Thêm mới
-                            </a>
-                        </li>
-
-                    </ul>			
-                </li>
-
-                <li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"/></svg> Cấu hình</a></li>
-
-                <li role="presentation" class="divider"></li>
-                <li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Đăng xuất</a></li>
-            </ul>
-
-        </div><!--/.sidebar-->
-
-        <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+		
             <div class="row">
                 <ol class="breadcrumb">
                     <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
@@ -138,7 +18,7 @@
                     <div class="panel panel-default">
 
                         <div class="panel-body" style="position: relative;">
-                            <a href="#" class="btn btn-primary" style="margin: 10px 0 20px 0; position: absolute;">Thêm danh mục mới</a>
+                            <a href="quantri.php?page_layout=themdm" class="btn btn-primary" style="margin: 10px 0 20px 0; position: absolute;">Thêm danh mục mới</a>
                             <table data-toggle="table" data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-sort-name="name" data-sort-order="desc">
                                 <thead>
                                     <tr>						        
@@ -151,9 +31,9 @@
                                 <tbody>
                                     <tr>
                                         <td data-checkbox="true">1</td>
-                                        <td data-checkbox="true"><a href="#">iPhone</a></td>						        
+                                        <td data-checkbox="true"><a href="quantri.php?page_layout=suadm">iPhone</a></td>						        
                                         <td>
-                                            <a href="#"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
+                                            <a href="quantri.php?page_layout=suadm"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
                                         </td>
 
                                         <td>
@@ -225,33 +105,4 @@
 
 
 
-        </div><!--/.main-->
-
-        <script src="js/jquery-1.11.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/chart.min.js"></script>
-        <script src="js/chart-data.js"></script>
-        <script src="js/easypiechart.js"></script>
-        <script src="js/easypiechart-data.js"></script>
-        <script src="js/bootstrap-datepicker.js"></script>
-        <script src="js/bootstrap-table.js"></script>
-        <script>
-            !function ($) {
-                $(document).on("click", "ul.nav li.parent > a > span.icon", function () {
-                    $(this).find('em:first').toggleClass("glyphicon-minus");
-                });
-                $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
-            }(window.jQuery);
-
-            $(window).on('resize', function () {
-                if ($(window).width() > 768)
-                    $('#sidebar-collapse').collapse('show')
-            })
-            $(window).on('resize', function () {
-                if ($(window).width() <= 767)
-                    $('#sidebar-collapse').collapse('hide')
-            })
-        </script>	
-    </body>
-
-</html>
+       
