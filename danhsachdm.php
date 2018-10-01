546a6512c87cbf9822ea5a05c7ccd1791903f2ed
- <->
2018-10-02 00:42:35 +0700

    Update trainning project: Back end_L04

--- danhsachdm.php
+++ danhsachdm.php
@@ -1,108 +1,74 @@
-		
-            <div class="row">
-                <ol class="breadcrumb">
-                    <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-                    <li class="active"></li>
-                </ol>
-            </div><!--/.row-->
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Quản lý danh mục</h1>
-                </div>
-            </div><!--/.row-->
+<?php
+    $sql = "SELECT * FROM dmsanpham ORDER BY id_dm ASC";
+    $query = mysqli_query($conn, $sql);
+?>		
 
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
+<div class="row">
+    <ol class="breadcrumb">
+        <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+        <li class="active"></li>
+    </ol>
+</div><!--/.row-->
 
-                        <div class="panel-body" style="position: relative;">
-                            <a href="quantri.php?page_layout=themdm" class="btn btn-primary" style="margin: 10px 0 20px 0; position: absolute;">Thêm danh mục mới</a>
-                            <table data-toggle="table" data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-sort-name="name" data-sort-order="desc">
-                                <thead>
-                                    <tr>						        
-                                        <th data-sortable="true">ID</th>
-                                        <th data-sortable="true">Tên danh mục</th>
-                                        <th data-sortable="true">Sửa</th>
-                                        <th data-sortable="true">Xóa</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td data-checkbox="true">1</td>
-                                        <td data-checkbox="true"><a href="quantri.php?page_layout=suadm">iPhone</a></td>						        
-                                        <td>
-                                            <a href="quantri.php?page_layout=suadm"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
-                                        </td>
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Quản lý danh mục</h1>
+    </div>
+</div><!--/.row-->
 
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td data-checkbox="true">2</td>
-                                        <td data-checkbox="true"><a href="#">Samsung</a></td>						        
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
-                                        </td>
 
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td data-checkbox="true">3</td>
-                                        <td data-checkbox="true"><a href="#">Oppo</a></td>						        
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
-                                        </td>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
 
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td data-checkbox="true">4</td>
-                                        <td data-checkbox="true"><a href="#">Sony</a></td>						        
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
-                                        </td>
+            <div class="panel-body" style="position: relative;">
+                <a href="quantri.php?page_layout=themdm" class="btn btn-primary" style="margin: 10px 0 20px 0; position: absolute;">Thêm danh mục mới</a>
+                <table data-toggle="table" data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-sort-name="name" data-sort-order="desc">
+                    <thead>
+                        <tr>						        
+                            <th data-sortable="true">ID</th>
+                            <th data-sortable="true">Tên danh mục</th>
+                            <th data-sortable="true">Sửa</th>
+                            <th data-sortable="true">Xóa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($row = mysqli_fetch_array($query)){
+                        ?>
+                        <tr>
+                            <td data-checkbox="true"><?php echo $row['id_dm']; ?></td>
+                            <td data-checkbox="true"><a href="quantri.php?page_layout=suadm"><?php echo $row['ten_dm']; ?></a></td>						        
+                            <td>
+                                <a href="quantri.php?page_layout=suadm"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
+                            </td>
 
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td data-checkbox="true">5</td>
-                                        <td data-checkbox="true"><a href="#">HTC</a></td>						        
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked brush" style="width: 20px;height: 20px;"><use xlink:href="#stroked-brush"/></svg></span></a>
-                                        </td>
+                            <td>
+                                <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
+                            </td>
+                        </tr>
+                        <?php 
+                            }
+                        ?>
 
-                                        <td>
-                                            <a href="#"><span><svg class="glyph stroked cancel" style="width: 20px;height: 20px;"><use xlink:href="#stroked-cancel"/></svg></span></a>
-                                        </td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
-                            <ul class="pagination" style="float: right;">
-                                <li><a href="#"><<</a></li>
-                                <li><a href="#"><</a></li>
-                                <li><a href="#">1</a></li>
-                                <li class="active"><a href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">4</a></li>
-                                <li><a href="#">5</a></li>
-                                <li><a href="#">></a></li>
-                                <li><a href="#">>></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div><!--/.row-->	
+                    </tbody>
+                </table>
+                <ul class="pagination" style="float: right;">
+                    <li><a href="#"><<</a></li>
+                    <li><a href="#"><</a></li>
+                    <li><a href="#">1</a></li>
+                    <li class="active"><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">></a></li>
+                    <li><a href="#">>></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->	
 
 
 
-       
